ea418a50666cfddb5450f4c0701978b14d821740
Gauthier Deroo <g.deroo@gmail.com>
2018-12-20 22:35:57 +0100

    feat Update Bussiness card with my personal data

--- bin/card.js
+++ bin/card.js
@@ -14,14 +14,14 @@ const options = {
 
 // Text + chalk definitions
 const data = {
-  name: chalk.white('Tierney Cyren /'),
-  handle: chalk.cyan('bitandbang'),
-  work: chalk.white('Senior Cloud Developer Advocate at Microsoft'),
-  twitter: chalk.cyan('https://twitter.com/bitandbang'),
-  github: chalk.cyan('https://github.com/bnb'),
-  linkedin: chalk.cyan('https://linkedin.com/in/bitandbang'),
-  web: chalk.cyan('https://bnb.im'),
-  npx: chalk.white('npx bitandbang'),
+  name: chalk.white('Gauthier Deroo /'),
+  handle: chalk.cyan('gauthierd-'),
+  work: chalk.white('Fullstack Javascript developer'),
+  twitter: chalk.cyan('https://twitter.com/gauthierderoo'),
+  github: chalk.cyan('https://github.com/GauthierD-'),
+  linkedin: chalk.cyan('https://linkedin.com/in/gauthierderoo'),
+  web: chalk.cyan('https://about.me/gauthierd'),
+  npx: chalk.white('npx gauthierd-'),
   labelWork: chalk.white.bold('      Work:'),
   labelTwitter: chalk.white.bold('   Twitter:'),
   labelGitHub: chalk.white.bold('    GitHub:'),
